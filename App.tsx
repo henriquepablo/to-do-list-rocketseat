4bbaac16270a0fe6fee088ea1acddf74977f2d66
henriquepablo <pablo269704@gmail.com>
2025-04-05 16:15:18 -0300

    chore: add colors and create screen home

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet} from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './src/styles';
+import Home from './src/screens/home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   );
 }
 
---
+++ src/screens/home/index.tsx
@@ -0,0 +1,13 @@
+import { View } from "react-native";
+import { theme } from "../../styles";
+
+function Home() {
+
+    return(
+        <View style={{flex: 1, backgroundColor: theme.colors.gray_600}}>
+
+        </View>
+    )
+}
+
+export default Home;
